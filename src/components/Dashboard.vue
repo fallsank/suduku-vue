15b8aa7f6bab91db51a2e69f369ad6dd598b90b9
fallsank <fallsank@163.com>
2019-01-21 16:23:03 +0800

    添加数独底部按钮界面

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="dashboard-wrapper">
+    <button class="dash-button">新游戏</button>
+    <button class="dash-button">清空</button>
+  </div>
+</template>
+
+<style lang="scss">
+.dashboard-wrapper {
+  position: absolute;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  height: 42px;
+  background: #eee;
+
+  box-sizing: border-box;
+
+  button.dash-button {
+    height: 100%;
+    color: rgb(56, 54, 54);
+    background-color: #f4f7f7;
+    border: 1px solid #dae6e0;
+    width: 50%;
+    font-size: 16px;
+    font-weight: 500;
+  }
+}
+</style>
