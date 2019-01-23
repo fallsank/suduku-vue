cdbcb218d687b8a5a19fef5868ae229390045647
fallsank <fallsank@163.com>
2019-01-23 17:05:19 +0800

    添加宫坐标、点坐标相互转换的方法

--- src/assets/js/utils.js
+++ src/assets/js/utils.js
@@ -9,8 +9,43 @@ export default {
     }
     return arr;
   },
-  // 检测输入数字是否合法
-  checkSudokuNum() {
-    return true;
+  /**
+   * 根据宫th获取所有坐标点
+   * @param boxIndex
+   */
+  queryPointByBox(boxIndex) {
+    const points = [];
+    for (let cellIndex = 0; cellIndex < 9; cellIndex++) {
+      const point = this.convertBoxToPoint(boxIndex, cellIndex);
+      points.push({
+        rowIndex: point.rowIndex,
+        colIndex: point.colIndex
+      });
+    }
+    return points;
+  },
+  /**
+   * 将宫th/偏移量转换为点坐标
+   * @param boxIndex 宫th
+   * @param cellIndex 偏移量
+   * @returns {{colIndex: *, rowIndex: *}}
+   */
+  convertBoxToPoint(boxIndex, cellIndex) {
+    return {
+      rowIndex: Math.floor(boxIndex / 3) * 3 + Math.floor(cellIndex / 3),
+      colIndex: (boxIndex % 3) * 3 + (cellIndex % 3)
+    };
+  },
+  /**
+   * 将点坐标转换为宫th/偏移量
+   * @param rowIndex 行坐标
+   * @param colIndex 列坐标
+   * @returns {{cellIndex: *, boxIndex: *}}
+   */
+  convertPointToBox(rowIndex, colIndex) {
+    return {
+      boxIndex: Math.floor(rowIndex / 3) * 3 + Math.floor(colIndex / 3),
+      cellIndex: (rowIndex % 3) * 3 + (colIndex % 3)
+    };
   }
 };
